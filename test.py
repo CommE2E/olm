ea1ace8e1ead7374270f0b7c7bc9a1ea24b4dafa
Mark Haines <mark.haines@matrix.org>
2015-02-26 17:10:43 +0000

    Put the #! in the right place

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+#! /usr/bin/python
 # Copyright 2015 OpenMarket Ltd
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#! /usr/bin/python
 import subprocess
 import glob
 import os
