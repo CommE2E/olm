900d2d70b47ce299222ee27f2ed6374d2a004aec
Mark Haines <mark.haines@matrix.org>
2015-08-19 15:38:20 +0100

    Add docstring for the base64 encode/decode methods

--- include/olm/base64.hh
+++ include/olm/base64.hh
@@ -20,30 +20,44 @@
 
 namespace olm {
 
-
+/**
+ * The number of bytes of unpadded base64 needed to encode a length of input.
+ */
 static std::size_t encode_base64_length(
     std::size_t input_length
 ) {
     return 4 * ((input_length + 2) / 3) + (input_length + 2) % 3 - 2;
 }
 
-
+/**
+ * Encode the raw input as unpadded base64.
+ * Writes encode_base64_length(input_length) bytes to the output buffer.
+ * The input can overlap with the last three quarters of the output buffer.
+ * That is, the input pointer may be output + output_length - input_length.
+ */
 std::uint8_t * encode_base64(
     std::uint8_t const * input, std::size_t input_length,
     std::uint8_t * output
 );
 
-
+/**
+ * The number of bytes of raw data a length of unpadded base64 will encode to.
+ * Returns std::size_t(-1) if the length is not a valid length for base64.
+ */
 std::size_t decode_base64_length(
     std::size_t input_length
 );
 
-
+/**
+ * Decodes the unpadded base64 input to raw bytes.
+ * Writes decode_base64_length(input_length) bytes to the output buffer.
+ * The output can overlap with the first three quarters of the input buffer.
+ * That is, the input pointers and output pointer may be the same.
+ */
 std::uint8_t const * decode_base64(
     std::uint8_t const * input, std::size_t input_length,
     std::uint8_t * output
 );
-
 
 } // namespace olm
 
