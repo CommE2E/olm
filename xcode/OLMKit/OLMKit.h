f29eabde8b42cde1d5054df8977dd719099575ae
manuroe <manu@matrix.org>
2016-09-28 16:06:45 +0200

    OLMKit: Use the same version as libolm

--- xcode/OLMKit/OLMKit.h
+++ xcode/OLMKit/OLMKit.h
@@ -8,14 +8,10 @@
 
 #import <UIKit/UIKit.h>
 
-//! Project version number for OLMKit.
-FOUNDATION_EXPORT double OLMKitVersionNumber;
-
-//! Project version string for OLMKit.
-FOUNDATION_EXPORT const unsigned char OLMKitVersionString[];
+//! Project version string for OLMKit, the same as libolm.
+NSString *OLMKitVersionString();
 
 // In this header, you should import all the public headers of your framework using statements like #import <OLMKit/PublicHeader.h>
-
 
 #import <OLMKit/OLMAccount.h>
 #import <OLMKit/OLMSession.h>
