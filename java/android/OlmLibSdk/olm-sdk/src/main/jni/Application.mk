71f57b79e57ca668c4621e1cecf5742f428c5cde
pedroGitt <pedro.contreiras@amdocs.com>
2016-10-21 19:03:18 +0200

    Update android platform to 16

--- java/android/OlmLibSdk/olm-sdk/src/main/jni/Application.mk
+++ java/android/OlmLibSdk/olm-sdk/src/main/jni/Application.mk
@@ -1,3 +1,3 @@
-APP_PLATFORM := android-21
-APP_ABI := arm64-v8a armeabi-v7a armeabi x86 x86_64
+APP_PLATFORM := android-16
+APP_ABI := arm64-v8a armeabi-v7a x86 x86_64 armeabi
 APP_STL := gnustl_static
